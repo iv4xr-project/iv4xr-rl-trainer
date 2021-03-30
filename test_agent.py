4bfda198be4b58522aec8775a3484cfa17048df0
Alexandre Kazmierowski <alexandre.kazmierowski@thalesgroup.com>
2021-03-30 08:47:54 +0200

    Add some logs for the first demo with MAEV

--- test_agent.py
+++ test_agent.py
@@ -1,11 +1,15 @@
 from connector.policy_server import PolicyServer, DisconnectEnvException
 import coloredlogs
 import logging
+import numpy as np
 
 
 logger = logging.getLogger('policy_server')
 coloredlogs.install(level='INFO')
 
+
+def normalize_obs(obs, obs_space):
+    return (np.array(obs) - obs_space.low) / (obs_space.high - obs_space.low)
 
 def main():
     PORT = 5555
@@ -18,16 +22,19 @@ def main():
 
     print("Environment initialized")
     print(server.env_spec.id)
-    print(server.action_space)
-    print(server.observation_space)
+    print(f"Action space: {server.action_space}")
+    print(f"State space: {server.observation_space}")
 
     try:
         while True:
             state = server.poll_state()
-            # action = agent.get_action(state)
             action = server.action_space.sample().tolist()
+            #action = [138500, 441747]
+            print(f"Taking action: {action}")
+            # action = agent.get_action(state)
             server.send_action(action)
             next_state, reward, done, info = server.poll_returns()
+            print(f"Current state: {normalize_obs(next_state['rawObservation'], server.observation_space)}")
             # replay_buffer.push((state, action, next_state, reward, done))
     except DisconnectEnvException:
         server.stop()
